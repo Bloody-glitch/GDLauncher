cd160fe824fff6f45ed727117b97086eabf9bdde
Bloody-glitch <59135458+Bloody-glitch@users.noreply.github.com>
2019-12-27 15:39:02 +0300

    Update launching.js

--- js/launching.js
+++ js/launching.js
@@ -1,5 +1,8 @@
+//import config.json
 const config = require('./../config/config.json');
+//import Geometry Dash Folder
 const gd = config.gdFolder
+//import child_process
 const child_process = require('child_process');
         
     let runGD = function(){
@@ -7,6 +10,7 @@ const child_process = require('child_process');
         let menu1 = document.getElementById('menuButton1');
         let menu2 = document.getElementById('menuButton2');
         let menu3 = document.getElementById('menuButton3');
+        //launch GeometryDash.exe from folder and blocked the buttons
         let workerProcess = child_process.exec('start GeometryDash.exe', {cwd: gd});
             button.innerHTML = "Now playing...";
             button.disabled = true; 
@@ -15,6 +19,7 @@ const child_process = require('child_process');
             menu3.disabled = true;
 
         workerProcess.on('close', function (code) {
+            //if GeometryDashexe is closed buttons unlocked
             button.innerHTML = "Play Geometry Dash";
             button.disabled = false;
             menu1.disabled = false;
@@ -22,23 +27,26 @@ const child_process = require('child_process');
             menu3.disabled = false;
         });
     }
-
+        
+    //launch FPSBypass.exe
     let runBypass = function(){
-        let workerProcess = child_process.exec('start FPSBypass.exe', {cwd: "hacks/"}) 
+        let workerProcess = child_process.exec('start FPSBypass.exe', {cwd: "hacks/"}) //use resources/app/hacks/ when compiling
         workerProcess.on('close', function (code) {
         console.log(code);
         });
     }
-
+        
+    //launch MultiScaling.ex
     let runMS = function(){
-        let workerProcess = child_process.exec('start MultiScaling.exe', {cwd: "hacks/"}) 
+        let workerProcess = child_process.exec('start MultiScaling.exe', {cwd: "hacks/"}) //use resources/app/hacks/ when compiling
         workerProcess.on('close', function (code) {
         console.log(code);
         });
     }
-
+        
+    //launch PracticeMusic.exe
     let runPM = function(){
-        let workerProcess = child_process.exec('start PracticeMusic.exe', {cwd: "hacks/"}) 
+        let workerProcess = child_process.exec('start PracticeMusic.exe', {cwd: "hacks/"}) //use resources/app/hacks/ when compiling
         workerProcess.on('close', function (code) {
         console.log(code);
         }); 
