7350afdc3e8fb04f755b237cad46d6c9767666f2
Bloody-glitch <59135458+Bloody-glitch@users.noreply.github.com>
2019-12-27 15:27:14 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const url = require('url');
+const {app, BrowserWindow, Menu} = require('electron');
+const client = require('discord-rich-presence')('658606532761485323');
+       client.updatePresence({
+        details: 'Playing Geometry Dash',
+        startTimestamp: Date.now(),
+        largeImageKey: 'gd',
+        instance: true
+      });
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        name: "GD Launcher",
+        width: 1176,
+        height: 720,
+        minWidth: 1176,
+        minHeight: 720,
+        toolbar: false,
+        icon: path.join(__dirname, '/img/GeometryDash.ico')
+    });
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate([{ label: 'Init' }]))
+
+    win.webContents.once('did-finish-load', () => {
+        Menu.setApplicationMenu(null)
+      })
+      
+    win.setMenu(null)
+    
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '/html/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+   win.on('closed', () => {
+        win = null;
+   });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
